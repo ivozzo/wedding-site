4e2db3a3e6fcbd2be93eb3954d59f76447ac4870
Alessandro <alessandro.ivaldi@email.it>
2017-07-25 11:58:27 +0200

    Added login function file

---
+++ server/Server/utilities/login.js
@@ -0,0 +1,24 @@
+const mongodbtools = require('./mongodb-tools');
+
+module.exports = {
+    checkLogin: function (sess, res, page) {
+        check_session(sess, res, page);
+    }
+}
+
+function check_session(sess, res, page) {
+    if (sess.username) {
+        console.log(`User session found: %s`, sess.username);
+
+        res.render(page, {
+            notification: notification,
+        });
+    } else {
+        mongodbtools.checkInitialized(function (err, response) {
+            if (err) {
+                console.log(`Cannot check if the database has already been initialized`);
+            }
+        });
+        res.redirect('/login');
+    }
+}
