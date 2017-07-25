eb1e38db7096798e9a1df9b50891cacd8c538226
Alessandro <alessandro.ivaldi@email.it>
2017-07-25 12:52:14 +0200

    User router and dedicated module

---
+++ server/Server/routers/router_user.js
@@ -0,0 +1,24 @@
+// Loading Modules
+const express = require('express'),
+    router = express.Router(),
+    user_tools = require('../utilities/user'),
+    login_tools = require('../utilities/login');
+
+// Splash page
+router.get('/', function (req, res) {
+    console.log(`GET: /guest`);
+    login_tools.checkLogin(req.session, res, 'guest.pug');
+});
+
+// User addition
+router.post('/insert', function (req, res) {
+    console.log(`POST: /user/insert`);
+    user_tools.insertUser(req, res);
+});
+
+router.post('/update', function (req, res) {
+    console.log(`POST: /user/update`);
+    user_tools.updateUser(req,res);
+});
+
+module.exports = router;
---
+++ server/Server/utilities/user.js
@@ -0,0 +1,102 @@
+module.exports = {
+    insertUser: function (req, res) {
+        insert_User(req, res);
+    },
+
+    updateUser: function (req, res) {
+        update_User(req, res);
+    }
+}
+
+/**
+ * Insert user on database
+ * @function insert_User
+ * @param  {Request} req
+ * @param  {Response} res
+ */
+function insert_User(req, res) {
+    var error_user;
+
+    var user = objects.User(
+        req.body.Name,
+        req.body.Surname,
+        req.body.Email,
+        req.body.Username,
+        req.body.Password);
+
+    console.log(`Got an user creation request`);
+
+    //add the user to the collection
+    mongodbtools.createUser(user, function (err, response) {
+        if (err) {
+            console.log("Cannot create User object on database")
+            error_user = err;
+        } else {
+            console.log("User has been correctly inserted");
+        }
+    });
+
+    //if an error has happened prepare the error notification
+    if (error_user === true) {
+        notification.show = true;
+        notification.error = true;
+        notification.message = `Impossibile creare l'utente.`;
+    } else {
+        notification.show = true;
+        notification.error = false;
+        notification.message = `Utente creato con successo.`;
+    }
+
+    res.render('user.pug', {
+        notification: notification
+    });
+}
+
+/**
+ * Update user on database
+ * @function update_User
+ * @param  {Request} req
+ * @param  {Response} res
+ */
+function update_User(req, res) {
+    var error_user;
+
+    var new_password = req.body.newPassword;
+
+    var user = objects.User(
+        "",
+        "",
+        "",
+        req.body.Username,
+        req.body.oldPassword
+    )
+
+    var old_password = user.password;
+
+    mongodbtools.findUser(user, function (err, response) {
+        if (err) {
+            //TODO add notification error
+            console.log(`No user found, redirecting to login`);
+        }
+        console.log(old_password)
+        console.log(new_password)
+
+        if (response.users[0].login.user === req.body.Username) {
+            if (response.users[0].login.password === old_password) {
+
+                var updatedUser = response.users[0];
+                updatedUser.login.password = objects.cryptPassword(new_password);
+
+                mongodbtools.updateUser(updatedUser, function (err, response) {
+
+                });
+            } else {
+                //TODO add notification error
+                console.log(`User %s found but password not correct`, user.username);
+                res.render('user.pug', {
+                    notification: notification
+                });
+            }
+        }
+    });
+}
